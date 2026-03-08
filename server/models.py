3c4a2cffe147c21a905e925e1f95e766667be903
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-08 16:01:17 -0400

    User Model created

--- server/models.py
+++ server/models.py
@@ -4,3 +4,26 @@ from marshmallow import Schema, fields
 
 from config import db, bcrypt
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
