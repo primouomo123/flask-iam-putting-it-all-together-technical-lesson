692f987008dac98f13a9c031a0472baae93ba1a5
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-08 16:12:45 -0400

    Post Model created

--- server/models.py
+++ server/models.py
@@ -27,3 +27,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    user = db.relationship('User', back_populates="posts")
+
+    def __repr__(self):
+        return f'<Post {self.id}: {self.content}>'
