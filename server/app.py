34cbfb3b22cf746d2ece9a73089dd6edc2294c99
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-08 17:19:38 -0400

    Importing the missing models

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
+from models import User, Post, UserSchema, PostSchema
+
 from config import app, db, api
 
 if __name__ == '__main__':
